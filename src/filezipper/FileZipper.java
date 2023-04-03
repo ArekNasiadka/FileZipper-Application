f0829d7ee9910a5235160d5ad68a2be12c1d69af
ArekNasiadka <nasiadka.arkadiusz@gmail.com>
2023-04-03 18:56:07 +0200

    Create function of archiving and compressing files and folders

--- src/filezipper/FileZipper.java
+++ src/filezipper/FileZipper.java
@@ -1,8 +1,15 @@
 package filezipper;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import javax.swing.*;
 
 public class FileZipper extends JFrame
@@ -108,7 +115,7 @@ public class FileZipper extends JFrame
             else if (e.getActionCommand().equals("Remove"))
                 removeEntriesFromList();
             else if (e.getActionCommand().equals("Zip"))
-                System.out.println("Zip");
+                createZipArchive();
         }
     
         private void addEntriesToArchive()
@@ -142,5 +149,80 @@ public class FileZipper extends JFrame
             for (int i = 0; i < tmp.length; i++)
                 listModel.remove(tmp[i]-i);
         }
+        
+        private void createZipArchive()
+        {
+            chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            chooser.setSelectedFile(new File(System.getProperty("user.dir")+File.separator+"myname.zip"));
+            int tmp = chooser.showDialog(rootPane, "Compress");
+            
+            if (tmp == JFileChooser.APPROVE_OPTION)
+            {      
+       
+                    byte tmpData[] = new byte[BUFFOR];
+                    try
+                    {
+                     ZipOutputStream zOutS = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(chooser.getSelectedFile()),BUFFOR));
+
+                     for (int i = 0; i < listModel.getSize(); i++)
+                     {
+                         if (!((File)listModel.get(i)).isDirectory())
+                             zipp(zOutS, (File)listModel.get(i), tmpData, ((File)listModel.get(i)).getPath());
+                         else
+                         {
+                             writePaths((File)listModel.get(i));
+                             
+                             for (int j = 0; j < pathsList.size(); j++)
+                                 zipp(zOutS, (File)pathsList.get(j), tmpData, ((File)listModel.get(i)).getPath());
+                             
+                                 pathsList.removeAll(pathsList);
+                         } 
+                     }
+
+                     zOutS.close();
+                    }
+                    catch(IOException e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+            }
+        }
+       private void zipp(ZipOutputStream zOutS,File filePath, byte[] tmpData, String basePath) throws IOException
+       {
+             BufferedInputStream inS = new BufferedInputStream(new FileInputStream(filePath), BUFFOR);
+
+             zOutS.putNextEntry(new ZipEntry(filePath.getPath().substring(basePath.lastIndexOf(File.separator)+1)));
+
+             int counter;
+             while ((counter = inS.read(tmpData, 0, BUFFOR)) != -1)
+             zOutS.write(tmpData, 0, counter);
+
+
+             zOutS.closeEntry();
+ 
+             inS.close();
+       }
+       public static final int BUFFOR = 1024;
+       
+          private void writePaths(File pathName)
+    {
+       String[] fileCatalogName = pathName.list();
+       
+       for (int i = 0; i < fileCatalogName.length; i++)
+       {
+           File p = new File(pathName.getPath(), fileCatalogName[i]);
+          
+           if (p.isFile())
+               pathsList.add(p);
+           
+           if (p.isDirectory())
+               writePaths(new File(p.getPath()));
+              
+          
+       }
     }
+      
+     ArrayList pathsList = new ArrayList();
+    }
+    
 }
